372e02cd6d34b07c6fe7a3cbdc0c10d5ad6e9789
Vitalij Stinskij <38437905+vitalmix@users.noreply.github.com>
2019-12-03 21:59:55 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
-
 import (
-	"log"
 	"net/http"
+	"log"
+	"html/template"
+	_ "github.com/lib/pq"
+	"database/sql"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"fmt"
 )
-
-func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+var db *sql.DB
+func rollHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("simple_list.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		books, err := dbGetBooks()
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, books)
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+}
+func addBookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("simple_form.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		name := r.Form.Get("name")
+		year := r.Form.Get("year")
+		length := r.Form.Get("length")
+		err := dbAddBook(name, year, length)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "4747"
+		fmt.Println(port)
+	}
+	return ":" + port
+}
+func main() {
+	err := dbConnect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", rollHandler)
+	http.HandleFunc("/add", addBookHandler)
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
 }
